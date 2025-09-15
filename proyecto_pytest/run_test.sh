73b9f4f76f875bf476d103ec6f49162614ca5620
Pablo Quiroz <pabloskiquiroz@gmail.com>
2025-09-15 10:41:34 -0300

    add pip upgrade test script

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-echo "creando entorno virtual"
-python3 -m venv venv
+echo "creando entorno virtual o activando entorno virtual"
+if [ !-d "venv" ]; then
+    python3 -m venv venv 
+fi
 
 echo "activando entorno virtual"
 source venv/bin/activate
 
 echo "instalando dependencias"
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "corriendo pruebas con pytest"
 pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
