a275f5b9981a2528ac864a50ba5c5318618ff10a
Pablo Quiroz <pabloskiquiroz@gmail.com>
2025-09-15 10:49:32 -0300

    check sintaxt error

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "creando entorno virtual o activando entorno virtual"
+echo " creando entorno virtual o activando entorno virtual"
 
 if [ !-d "venv" ]; then
     python3 -m venv venv 
 fi
 
-echo "activando entorno virtual"
+echo " activando entorno virtual"
 source venv/bin/activate
 
-echo "instalando dependencias"
+echo " instalando dependencias"
 pip install --upgrade pip --break-system-packages
 pip install -r requirements.txt --break-system-packages
 
-echo "corriendo pruebas con pytest"
+echo " corriendo pruebas con pytest"
 pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
-echo "pruebas finalizadas resultados en reports"
+echo " pruebas finalizadas resultados en reports"
 
 source venv/bin/deactivate
 
